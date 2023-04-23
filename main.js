41c9ae3d5b461193103bddd7f953d2ce01a036d5
imaad <imaadmalik2@gmail.com>
2023-04-22 20:48:54 -0400

    Final changes

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ let confirmPassword = document.querySelector('#pass-true');
 function validatePassword() {
     if(password.value != confirmPassword.value) {
         confirmPassword.setCustomValidity(`Passwords don't match.`)
+        confirmPassword.style.borderColor = 'red';
     } else {
         confirmPassword.setCustomValidity('');
+        confirmPassword.style.borderColor = 'rgb(104, 104, 255)';
     }
 }
 
